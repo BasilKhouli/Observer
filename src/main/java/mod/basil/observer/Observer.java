99f1e3c993c5dbdb195d34fb431b4354797df64d
BasilKhouli <basil.khouli@gmail.com>
2022-11-10 14:45:11 +1300

    Initial commit

---
+++ src/main/java/mod/basil/observer/Observer.java
@@ -0,0 +1,7 @@
+package mod.basil.observer;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraft.client.Minecraft;
+@Mod(Observer.MODID)
+public class Observer {
+    public static final String MODID = "observer";
+}
